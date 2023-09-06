e90385519dd20d25b20dc8613a1e61df9ee1304e
saranpradeep-5c <saranp@5cnetwork.com>
2023-09-06 23:08:56 +0530

    Adding routes

---
+++ controllers/orderItemController.go
@@ -0,0 +1,31 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+func GetOrderItem() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc{
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateOrderItems() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+func UpdateOrderItems() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
---
+++ controllers/tableController.go
@@ -0,0 +1,25 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetTables() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+func GetTable() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func CreateTables() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
+func UpdateTables() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
